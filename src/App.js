c513f5d64ed1d3be836fa6e2f3664ba763200888
Divya Veladandi <divya.veladandi@otsi.com>
2023-11-23 15:52:55 +0530

    inital push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';  
+class App extends React.Component {  
+  
+    constructor(props){
+        super(props);
+        this.state={value:""};
+        this.handlechange= this.handlechange.bind(this);
+        this.handlesubmit=this.handlesubmit.bind(this);
+    }
+        handlechange(event){
+            this.setState({value:event.target.value});
+        }
+        handlesubmit(event){
+            alert('login in succeessfull'+this.state.value);
+        }
+        render(){
+            return(
+                <form onSubmit={this.handlesubmit}>
+                <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handlechange}/>
+                </label>
+                <input type="submit" value="submit"/>
+                </form>
+            )
+        }
+        
+    }
+    export default App
